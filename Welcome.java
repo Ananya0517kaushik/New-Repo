4842b4cb50bb79576f0f13f2f2ae0953fc3b3a25
ANANYA KAUSHIK <147698162+Ananya0517kaushik@users.noreply.github.com>
2024-12-30 03:30:13 -0800

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,5 @@
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Welcome to the CI/CD Pipeline Practical!");
+        System.out.println("Welcome to Devops practical!");
     }
 }
